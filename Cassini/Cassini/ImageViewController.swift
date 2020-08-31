741d93c6b9797a76c243bfd1892f219507b6ff1c
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2020-08-31 16:18:34 +0100

    Added variables
    Added variables for ImageViewController

--- Cassini/Cassini/ImageViewController.swift
+++ Cassini/Cassini/ImageViewController.swift
@@ -20,4 +20,26 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
     }
     @IBOutlet weak var spinner: UIActivityIndicatorView!
     
+    //MARK: Variables
+    var imageView = UIImageView()
+    var imageURL: URL? {
+        didSet {
+            image = nil
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView?.contentSize = imageView.frame.size
+            spinner?.stopAnimating()
+        }
+    }
+    
 }
