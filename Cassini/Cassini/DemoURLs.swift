08b05f865f1afec9bf4811f8afcb5611e800556f
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2020-09-09 19:37:31 +0100

    Added dictionary variable
    Added static dictionary variable to be populated using closure

--- Cassini/Cassini/DemoURLs.swift
+++ Cassini/Cassini/DemoURLs.swift
@@ -12,4 +12,6 @@ struct DemoURLs {
     static let stanford = Bundle.main.url(forResource: "oval", withExtension: "jpg")
 //    static let stanford = URL(string: "https://upload.wikimedia.org/wikipedia/commons/5/55/Stanford_Oval_September_2013_panorama.jpg")
     
+    static var NASA: Dictionary<String, URL> = {
+    }()
 }
