6102384742bed772ef480e4807e7206d06a30624
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2020-09-10 18:47:03 +0100

    Added Dictionary in Closure
    Added Dictionary<String, String> in closure

--- Cassini/Cassini/DemoURLs.swift
+++ Cassini/Cassini/DemoURLs.swift
@@ -13,5 +13,10 @@ struct DemoURLs {
 //    static let stanford = URL(string: "https://upload.wikimedia.org/wikipedia/commons/5/55/Stanford_Oval_September_2013_panorama.jpg")
     
     static var NASA: Dictionary<String, URL> = {
+        let NASAURLStrings = [
+            "Cassini" : "https://www.jpl.nasa.gov/images/cassini/20090202/pia03883-full.jpg",
+            "Earth" : "https://www.nasa.gov/sites/default/files/wave_earth_mosiac_3.jpg",
+            "Saturn" : "https://www.nasa.gov/sites/default/files/saturn_collage.jpg"
+        ]
     }()
 }
