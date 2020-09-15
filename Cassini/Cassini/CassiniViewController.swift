30b0dccc5aa9853d04352868b59925df7dcf7a27
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2020-09-15 19:08:12 +0100

    Added prepare() Method
    Added prepare(for:, sender:) method to prepare for segue after a url option is chosen

--- Cassini/Cassini/CassiniViewController.swift
+++ Cassini/Cassini/CassiniViewController.swift
@@ -17,4 +17,17 @@ class CassiniViewController: UIViewController {
     }
     
 
+    // MARK: Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            if let url = DemoURLs.NASA[identifier] {
+                if let imageVC = segue.destination.contents as? ImageViewController {
+                    imageVC.imageURL = url
+                    imageVC.title = (sender as? UIButton)?.currentTitle
+                }
+            }
+        }
+    }
+}
+
 }
